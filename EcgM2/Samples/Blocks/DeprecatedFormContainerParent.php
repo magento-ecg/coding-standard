0669c1976f6dbfd4cb18eb1f5234c069a77ae459
Aliaksandr Zhurauliou <1256891+zhooravlik@users.noreply.github.com>
2022-01-17 16:36:03 +0300

    Removed imperfect and glitchy Sniff DeprecatedParents.

--- EcgM2/Samples/Blocks/DeprecatedFormContainerParent.php
+++
@@ -1,6 +0,0 @@
-<?php
-namespace EcgM2\Samples\Blocks;
-
-class DeprecatedFormContainerParent extends \Magento\Backend\Block\Widget\Form\Container
-{
-}
--- EcgM2/Samples/Blocks/DeprecatedFormGenericParent.php
+++
@@ -1,6 +0,0 @@
-<?php
-namespace EcgM2\Samples\Blocks;
-
-class DeprecatedFormGenericParent extends \Magento\Backend\Block\Widget\Form\Generic
-{
-}
