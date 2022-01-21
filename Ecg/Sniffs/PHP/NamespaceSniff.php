ffd6409ad2a1997c441b000a11fa32db07ccdf08
Aliaksandr Zhurauliou <1256891+zhooravlik@users.noreply.github.com>
2022-01-21 16:47:13 +0300

    MAGINT-36: Up-to-Date Code Sniffer
    * Improvements for the NamespaceSniff (in case any imported Class's has the Exception ClassName in an Imported Class path).

--- Ecg/Sniffs/PHP/NamespaceSniff.php
+++ Ecg/Sniffs/PHP/NamespaceSniff.php
@@ -9,6 +9,13 @@ use PHP_CodeSniffer\Files\File;
 
 class NamespaceSniff implements Sniff
 {
+    const NAME_T_NS_NAMESPACE = 'T_NS_SEPARATOR';
+
+    private $tokens = [];
+
+    /**
+     * @return array|int[]|mixed[]
+     */
     public function register()
     {
         return [
@@ -16,26 +23,28 @@ class NamespaceSniff implements Sniff
         ];
     }
 
+    /**
+     * @param File $phpcsFile
+     * @param $stackPtr
+     * @return int|void
+     */
     public function process(File $phpcsFile, $stackPtr)
     {
         if ($phpcsFile->findNext(T_NAMESPACE, 0) === false) {
             return;
         }
 
-        $tokens = $phpcsFile->getTokens();
+        $this->tokens = $phpcsFile->getTokens();
 
         $endOfTryStatement = $phpcsFile->findEndOfStatement($stackPtr);
-
         $posOfCatchVariable = $phpcsFile->findNext(T_VARIABLE, $stackPtr, $endOfTryStatement);
-
         $posOfExceptionClassName = $phpcsFile->findNext(T_STRING, $stackPtr, $posOfCatchVariable);
-
         $posOfNsSeparator = $phpcsFile->findNext(T_NS_SEPARATOR, $stackPtr, $posOfExceptionClassName);
 
         if ($posOfNsSeparator === false) {
-            $exceptionClassName = trim($tokens[$posOfExceptionClassName]['content']);
-            $posOfClassInUse = $phpcsFile->findNext(T_STRING, 0, $stackPtr, false, $exceptionClassName);
-            if ($posOfClassInUse === false || $tokens[$posOfClassInUse]['level'] != 0) {
+            $exceptionClassName = trim($this->tokens[$posOfExceptionClassName]['content']);
+            $posOfClassInUse = $this->findNextClassName($phpcsFile, 0, $stackPtr, $exceptionClassName);
+            if ($posOfClassInUse === false || $this->tokens[$posOfClassInUse]['level'] !== 0) {
                 $phpcsFile->addError(
                     'Namespace for "' . $exceptionClassName . '" class is not specified.',
                     $posOfExceptionClassName,
@@ -43,5 +52,22 @@ class NamespaceSniff implements Sniff
                 );
             }
         }
+    }
+
+    /**
+     * @param File $phpcsFile
+     * @param int $startPtr
+     * @param int $endPtr
+     * @param string $exceptionClassName
+     * @return bool|int
+     */
+    private function findNextClassName(File $phpcsFile, $startPtr, $endPtr, $exceptionClassName)
+    {
+        $position = $phpcsFile->findNext(T_STRING, $startPtr, $endPtr, false, $exceptionClassName);
+        if ($this->tokens[$position + 1]['type'] === self::NAME_T_NS_NAMESPACE) {
+            $position = $this->findNextClassName($phpcsFile, $position + 1, $endPtr, $exceptionClassName);
+        }
+
+        return $position;
     }
 }
